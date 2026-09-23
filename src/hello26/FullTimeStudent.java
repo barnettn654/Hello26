defe41fe195fb535edb71eb5e8a42dabb7ba8d9d
Nicholas Barnett <barnettn@sheridancollege.ca>
2026-09-23 10:42:28 -0400

    added a new class called full time students

---
+++ src/hello26/FullTimeStudent.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello26;
+
+import Hello26.Student;
+
+/**
+ *
+ * @author alot
+ */
+public class FullTimeStudent extends Student {
+
+    public FullTimeStudent(String sID, String sName, Object ) {
+        super(sID, sName, <error>);
+    }
+    
+}
